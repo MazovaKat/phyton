ee87f93bea26233542a728ac72d6f4c70a7f58e0
EkaterinaMazova <kmazovs@bk.ru>
2022-12-08 18:41:52 +0300

    task3

---
+++ Lesson3/task3.py
@@ -0,0 +1,15 @@
+# my_list = [1.1, 1.2, 3.1, 5, 10.01]
+# new_list=[]
+
+# for i in my_list:
+#     new_list.append(str(float(i)).split('.'))
+    
+# print(new_list)
+
+# for item in new_list:
+#     for i in range(2):
+#         if item[1] != 0:
+#            item[0] = 0
+#            item[1] = item[1]
+           
+# print(new_list)
